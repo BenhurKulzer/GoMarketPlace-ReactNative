32e17a61e0c1f292a05d369224476a132eaa866b
Benhur Külzer <benhur.bitpc@gmail.com>
2020-04-29 18:51:52 -0300

    Desafio Finalizado

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,13 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    return formatValue(
+      products.reduce((a, { quantity, price }) => a + quantity * price, 0),
+    );
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((a, { quantity }) => a + quantity, 0);
   }, [products]);
 
   return (
